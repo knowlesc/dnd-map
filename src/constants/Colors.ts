a67c7a4fb280b48af87ae6146dba803fb65ace1a
knowlesc <knowlesc8@gmail.com>
2022-01-30 19:56:03 -0800

    colors

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,8 +1,7 @@
 export const Colors = [
-  "#28abe3",
-  "#01a14b",
-  "#231f20",
-  "#20a18b",
-  "#e1be2c",
-  "#ed1b24",
+  "#28abe3", // blueish
+  "#01C65D", // greenish
+  "#231f20", // black
+  "#e1be2c", // goldish
+  "#F57077", // reddish
 ];
